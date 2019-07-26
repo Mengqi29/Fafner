3be323cdc6cfc92223ae36782ec8873a363b5922
Mengqi29 <43154273+Mengqi29@users.noreply.github.com>
2019-07-25 23:43:57 -0400

    feat(assignment): Submit assignment of mission 2019-07-18
    This is my submission of mission 2019-07-18. It includes:
    
    1. Implement LinkedList.

---
+++ Assignments/2019-07-18/LinkedList/src/Coding/Training/Node.java
@@ -0,0 +1,29 @@
+package Coding.Training;
+
+public class Node {
+    Object value;
+    Node next;
+
+    public Node(Object value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+
+}
